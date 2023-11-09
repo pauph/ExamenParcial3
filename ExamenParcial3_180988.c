65abb7ed76481c4773562d76063a539d600f616a
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-11-09 15:38:49 -0600

    Add files via upload

---
+++ ExamenParcial3_180988.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define maxcolumnas 3200
+#define maxfilas 50
+#define maxlongitud 65001
+
+int main() {
+    FILE *archivoentrada = fopen("emails.csv", "r");
+    if (!archivoentrada) {
+        perror("Error al abrir el archivo CSV");
+        return 1;
+    }
+
+    char buffer[maxlongitud];
+    char *columnas[maxcolumnas];
+    int conteos[maxcolumnas] = {0};
+    int id = 988;
+
+    if (fgets(buffer, sizeof(buffer), archivoentrada)) {
+        char *token = strtok(buffer, ",");
+        int columna = 0;
+        while (token && columna < maxcolumnas) {
+            columnas[columna] = strdup(token);
+            token = strtok(NULL, ",");
+            columna++;
+        }
+    }
+
+    int filaActual;
+
+    for (filaActual = 0; filaActual <= id && fgets(buffer, sizeof(buffer), archivoentrada); ++filaActual) {
+    }
+
+    for (filaActual = 0; filaActual < maxfilas && fgets(buffer, sizeof(buffer), archivoentrada); ++filaActual) {
+        char *token = strtok(buffer, ",");
+        int columna = 0;
+        while (token && columna < maxcolumnas) {
+            conteos[columna] += atoi(token);
+            token = strtok(NULL, ",");
+            columna++;
+        }
+    }
+
+    fclose(archivoentrada);
+    int i;
+
+    FILE *archivosalida = fopen("180988.txt", "w");
+    if (!archivosalida) {
+        perror("Error al abrir el archivo de salida");
+        for (i = 0; i < maxcolumnas; i++) {
+            free(columnas[i]);
+        }
+        return 1;
+    }
+
+    for (i = 0; i < maxcolumnas && columnas[i] != NULL; i++) {
+        fprintf(archivosalida, "%s, %d\n", columnas[i], conteos[i]);
+        free(columnas[i]);
+    }
+
+    fclose(archivosalida);
+    printf("Archivo creado, el nombre es 180988.txt\n");
+
+    return 0;
+}
+
